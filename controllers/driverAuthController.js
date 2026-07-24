860a09332b2ada23c24038532ade89afc7eec923
shadab7477 <techshadab04@gmail.com>
2026-07-24 12:33:12 +0530

    asc

--- controllers/driverAuthController.js
+++ controllers/driverAuthController.js
@@ -1043,7 +1043,7 @@ export const getDriverProfile = async (req, res) => {
       name: driver.name,
       phone: driver.phone,
       email: driver.email,
-      profileimage:driver.profilePhoto.url,
+      profileimage: application?.profilePhoto?.url || null,
       isOnline: driver.isOnline,
       isAvailable: driver.isAvailable,
       lastActive: driver.lastActive,
